4ad0f4e99088864cc60b9ee4a897e38dc938aaaa
Albert Hilazo <albhilazo@gmail.com>
2016-05-28 21:26:42 +0200

    New AnalyzedItem object

---
+++ src/AppBundle/Domain/AnalyzedItem.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AppBundle\Domain;
+
+class AnalyzedItem
+{
+
+    private $type;
+    private $id;
+
+    public function __construct($type, $id)
+    {
+        $this->type = $type;
+        $this->id = $id;
+    }
+
+    public function type()
+    {
+        return $this->type;
+    }
+
+    public function id()
+    {
+        return $this->id;
+    }
+
+}
