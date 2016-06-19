d223a7db5594ee8dd9f8ee55e234398561a5a301
Albert Hilazo <albhilazo@gmail.com>
2016-06-19 20:10:00 +0200

    ItemRetriever service is now able to get product info from the Uvinum API

--- src/AppBundle/Services/ItemRetriever.php
+++ src/AppBundle/Services/ItemRetriever.php
@@ -4,19 +4,26 @@ namespace AppBundle\Services;
 
 use Doctrine\ORM\EntityManager;
 use AppBundle\Domain\ItemReference;
+use GuzzleHttp\Client;
 
 
 class ItemRetriever
 {
 
+    const BASE_UVINUM_API_URL = 'https://api.vcst.net';
+    const API_INSTANCE = 'uvinum';
+    const API_LANGUAGE = 'es_ES';
+
     private $entityManager;
+    private $api_key;
 
 
 
 
-    public function __construct(EntityManager $entityManager)
+    public function __construct(EntityManager $entityManager, $api_key)
     {
         $this->entityManager = $entityManager;
+        $this->api_key       = $api_key;
     }
 
 
@@ -29,11 +36,33 @@ class ItemRetriever
                 $repository = $this->entityManager->getRepository('AppBundle:Tweet');
                 break;
 
+            case 'uvinum-product':
+                return $this->getUvinumProduct($itemReference->id());
+                break;
+
             default:
                 return;
         }
 
         return $repository->find($itemReference->id());
+    }
+
+
+
+
+    private function getUvinumProduct($productId)
+    {
+        $http_client = new Client(['base_uri' => self::BASE_UVINUM_API_URL]);
+        $request_url = self::BASE_UVINUM_API_URL
+            . '/getProduct:p:' . $productId
+            . '?api_key=' . $this->api_key
+            . '&instance=' . self::API_INSTANCE
+            . '&language=' . self::API_LANGUAGE;
+
+        $response = (string) $http_client->get($request_url)->getBody();
+        $response = json_decode($response, true);
+
+        return $response['product_data'];
     }
 
 }
